9f5db9651592aa1d650e6dec8e2ea4b017440af0
nazeh <nuh@nuh.dev>
2026-05-12 12:54:21 +0300

    remove ethers, use reqwest instead, and debug! block response

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,37 @@
-use ethers::prelude::*;
-use std::convert::TryFrom;
+use serde_json::Value;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Rootstock Public RPC (Mainnet)
+    let client = reqwest::Client::new();
     let rpc_url = "https://public-node.rsk.co";
-    let provider = Provider::<Http>::try_from(rpc_url)?;
 
-    // Get the latest block number
-    let block_number = provider.get_block_number().await?;
-    println!("Latest Rootstock block: {}", block_number);
-
-    // Get the full block header
-    if let Some(block) = provider.get_block(block_number).await? {
-        println!("Block Hash: {:?}", block.hash);
-        println!("Parent Hash: {:?}", block.parent_hash);
+    async fn rpc_call(
+        client: &reqwest::Client,
+        url: &str,
+        method: &str,
+        params: Value,
+    ) -> Result<Value, Box<dyn std::error::Error>> {
+        let body = serde_json::json!({
+            "jsonrpc": "2.0",
+            "id": 1,
+            "method": method,
+            "params": params,
+        });
+        let mut resp: Value = client.post(url).json(&body).send().await?.json().await?;
+        Ok(resp["result"].take())
     }
+
+    let block_hex = rpc_call(&client, rpc_url, "eth_blockNumber", Value::Null)
+        .await?
+        .as_str()
+        .unwrap()
+        .to_string();
+    let block_num = u64::from_str_radix(block_hex.trim_start_matches("0x"), 16)?;
+    println!("Latest Rootstock block: {}\n", block_num);
+
+    let params = serde_json::json!([format!("{:#x}", block_num), false]);
+    let block = rpc_call(&client, rpc_url, "eth_getBlockByNumber", params).await?;
+    dbg!(&block);
 
     Ok(())
 }
